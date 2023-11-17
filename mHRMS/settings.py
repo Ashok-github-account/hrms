ea0943b00937635de27d8d4267fcab7a22dde927
ashok <chakravarthyashok41@gmail.com>
2023-11-17 10:05:24 +0530

    this is my third commit

--- mHRMS/settings.py
+++ mHRMS/settings.py
@@ -1,3 +1,4 @@
+
 from pathlib import Path
 import datetime
 import psycopg2
@@ -13,12 +14,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key_here')
+#SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key_here')
+SECRET_KEY = 'django-insecure-#cedo6k@mkcoy%!r2$wy$8h-(9x&ev+v%tvx+-k-o%)2jt$fb^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['52.66.240.147', 'web:8000', '172.31.9.5', 'localhost', '0.0.0.0']
 
 AUTH_USER_MODEL = "AdminAccount.User"
 
